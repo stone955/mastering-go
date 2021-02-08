924eb2e724ca4bd5a5aa254d705dbafe72daed5c
sidy <sidy5@chinaunicom.cn>
2021-02-08 09:29:45 +0800

    1.一致性哈希 2.uuid生成器

--- hash/hash_test.go
+++ hash/hash_test.go
@@ -1,1 +1,35 @@
 package hash
+
+import (
+	"math/rand"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestRing_Add(t *testing.T) {
+	ring := New(WithReplicas(3))
+	var nodes []string
+	for i := 0; i < 10; i++ {
+		nodes = append(nodes, "Node"+strconv.Itoa(i))
+	}
+	ring.Add(nodes...)
+
+	t.Logf("VirtualKeys = %v, \nRelations = %v\n",
+		ring.vKeys, ring.meta)
+}
+
+func TestRing_Get(t *testing.T) {
+	ring := New(WithReplicas(3))
+	var nodes []string
+	for i := 0; i < 10; i++ {
+		nodes = append(nodes, "Node"+strconv.Itoa(i))
+	}
+	ring.Add(nodes...)
+
+	r := rand.NewSource(time.Now().Unix())
+	for i := 0; i < 10; i++ {
+		key := strconv.FormatInt(r.Int63(), 10)
+		t.Logf("Node = %v\n", ring.Get(key))
+	}
+}
